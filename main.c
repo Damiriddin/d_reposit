e519044c46cc0e69795653441ef76d3f4e4e9bc8
Damiriddin <128897262+Damiriddin@users.noreply.github.com>
2023-03-25 12:52:32 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+ {   system("cls");
+
+    int a = getchar();
+    printf("%d",a);
+
+
+    printf("hello world");
+    return 0;
+}
